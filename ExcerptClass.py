91ce0b518f2d58874ce1033143060fe2cceb2d0c
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2016-11-23 01:55:02 -0600

    Updated ExcerptClass

--- ExcerptClass.py
+++ ExcerptClass.py
@@ -6,7 +6,7 @@ from pydub import AudioSegment
 import time, datetime
 import pandas as pd
 import numpy as np
-
+import subprocess
 
 
 
@@ -29,7 +29,10 @@ def create_tag_excerpt(row):
     start_msec = start * 1000.0
     duration_msec = duration * 1000
     clip_data = song[start_msec:start_msec+duration_msec]
-    clip_pathname="Excerpt_"+row['Pathname'][:-4]+"_start_"+str(start)+"_dur_"+str(duration)+'_class_'+str(row['Class'])+'.wav'
+    if len(row)==3:
+        clip_pathname=row['Out Directory']+row['Basename']+"_start_"+str(start)[:6]+"_dur_"+str(duration)[:6]+'_class_'+str(row['Class'])+'.wav'
+    elif len(row)>3:
+        clip_pathname=row['Out Directory']+row['Basename']+"_start_"+str(start)[:6]+"_dur_"+str(duration)[:6]+'_class_'+str(row['Class'])+'_label_'+str(row['Label'])+'.wav'
     clip_data=clip_data.set_channels(1)
     clip_data.export(clip_pathname, format="wav", parameters=["-ar 44100", "-acodec pcm_s16le"])
 
@@ -40,33 +43,42 @@ def create_tag_excerpt(row):
 
 def main(argv):
     inputfile = ''
-    outputfile = ''
     excerpt_class = 1
-    media_path=''
+    tag_path=''
     try:
-        opts, args = getopt.getopt(argv,"hi:m:o:e:v:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"hi:t:e:",["ifile=","ofile="])
     except getopt.GetoptError:
         print ""
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print "'"
+            print ""
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
-        elif opt in ("-o", "--ofile"):
-            outputfile = arg
         elif opt in ("-e", "--excerptclass"):
             excerpt_class = int(arg)
-        elif opt in ("-m", "--media"):
-            media_path=arg
+        elif opt in ("-t", "--tags"):
+            tag_path=arg
 
-    tag_data = pd.read_csv(inputfile,header=None)
+    tag_data = pd.read_csv(tag_path,header=None)
     if len(tag_data.iloc[0])==3:
         tag_data.columns=["Start","Class","Duration"]
     elif len(tag_data.iloc[0])==4:
-        tag_data.columns=["Start","Class","Duration","Value"]
-    tag_data['Pathname']=media_path
+        tag_data.columns=["Start","Class","Duration","Label"]
+        
+    wav_source=True
+    if inputfile.lower()[-4:]!='.wav':     # Creates a temporary WAV
+        wav_source=False                         # if input is MP3
+        temp_filename=inputfile.split('/')[-1]+'_temp.wav'
+        wav_path='/var/tmp/'+temp_filename   # Pathname for temp WAV
+        subprocess.call(['ffmpeg', '-y', '-i', inputfile, wav_path]) # '-y' option overwrites existing file if present
+    else:
+        wav_path=inputfile
+    
+    tag_data['Pathname']=wav_path
+    tag_data['Out Directory']='/'.join(inputfile.split('/')[:-1])+'/'
+    tag_data['Basename']=inputfile.split('/')[-1][:-4]
     
     tag_data_relevant=tag_data[tag_data['Class']==excerpt_class]
     
