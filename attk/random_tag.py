5eb3707445a844a6cb825cafe0fa76c7b368b9e9
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2017-06-02 22:05:30 -0500

    fixed bugs in randomTag and fixed module names

--- attk/random_tag.py
+++ attk/random_tag.py
@@ -15,8 +15,6 @@ from time import gmtime, strftime
 here = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(here)
 
-
-
 def media_duration(media_path):
     proc = subprocess.Popen(['ffprobe','-v','error','-show_entries','format=duration','-of','default=noprint_wrappers=1:nokey=1',media_path],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     duration = float(proc.stdout.read().strip())
@@ -49,25 +47,29 @@ def randomtag(media_path, out_dir, clip_secs=-1, num_tags=1, per_duration=-1, ex
         if counter > 1000000:
             print "*** Infinite loop error on: "+media_path
             break
-    return [(start,round(start+clip_secs,2)) for start in sorted(start_times)]  # list of 2-tuples: start and end times for random clips
+    tag_pairs = [(start,round(start+clip_secs,2)) for start in sorted(start_times)]
+    if extract==True:
+        tags_to_wav(media_path,out_dir,tag_pairs)
+    return tag_pairs  # list of 2-tuples: start and end times for random clips
 
 
 #python ExcerptClass.py -i /path/to/audio.mp3 -t /path/to/tags.csv -e 1 -o /path/to/output/directory
 
 
-def tags_to_csv(csv_filename,tag_table,class_num=0,class_label=''):
+def tags_to_csv(csv_filename,tag_pairs,class_num=0,class_label=''):
     with open(csv_filename, 'w') as csv_fo:
         csv_writer = csv.writer(csv_fo)
-        for start, end in tag_table:
+        for start, end in tag_pairs:
             if class_label=='':
                 csv_writer.writerow([start,class_num,end])
             else:
                 csv_writer.writerow([start,class_num,end,class_label])
-        if len(tag_table)>0:
+        if len(tag_pairs)>0:
             csv_fo.write('\n\n## RandomTags.py run by '+str(os.getlogin()))
             csv_fo.write('\n## '+strftime("%Y-%m-%d %H:%M:%S", gmtime())+' GMT\n')
 
-def tags_to_wav(media_path,basename,out_dir,tag_table):
+def tags_to_wav(media_path,out_dir,tag_pairs):
+    basename=os.path.splitext(os.path.basename(media_path))[0]
     wav_source=True
     if media_path.lower()[-4:] not in ('.mp3','.wav'):     # Creates a temporary WAV
         wav_source=False                         # if input is MP4
@@ -81,10 +83,12 @@ def tags_to_wav(media_path,basename,out_dir,tag_table):
             song = AudioSegment.from_mp3(audio_path)
         else:
             song = AudioSegment.from_wav(audio_path)
-    except:
-        print("Error loading audio with pyDub.")
-        return("Error loading audio with pyDub.")
-    for pair in tag_table:
+
+    except Exception as inst:
+        print(inst)
+        sys.exit(2)
+
+    for pair in tag_pairs:
         start = pair[0]
         duration = pair[1]-pair[0]
         clip_pathname=os.path.join(out_dir,basename+"_start_"+str(start)+"_dur_"+str(duration)+".wav")
@@ -94,10 +98,6 @@ def tags_to_wav(media_path,basename,out_dir,tag_table):
             clip_data = song[start_msec:start_msec+duration_msec]
             clip_data=clip_data.set_channels(1)
             clip_data.export(clip_pathname, format="wav")
-
-
-
-
 
 
 def main(argv):
@@ -110,9 +110,9 @@ def main(argv):
     per_duration=-1
     script_path=os.path.dirname(os.path.realpath(sys.argv[0]))
     try:
-        opts, args = getopt.getopt(argv[1:],"hi:s:n:p:o:e",["ifile="])
-    except getopt.GetoptError:
-        print ""
+        opts, args = getopt.getopt(argv[1:],"hei:s:n:p:o:",["ifile=","secs=","num=","per=","out="])
+    except Exception as inst:
+        print(inst)
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -128,17 +128,17 @@ def main(argv):
             per_duration = float(arg)
         elif opt in ("-o", "--out"):
             out_dir=arg
-        if ('-e' in sys.argv[1:])|('--extract' in sys.argv[1:]):
+        elif opt in ("-e", "--extract"):
             extract=True
-    basename=os.path.splitext(os.path.basename(media_path))[0]
-    csv_filename=basename+"|%ss_x%sec_random.csv"%(str(clip_secs),str(num_tags))
-    if per_duration > 0:
-        csv_filename=basename+"|%ssec_x%s_per_%ssec_random.csv"%(str(clip_secs),str(num_tags),str(per_duration))
-    tag_table=randomtag(media_path, out_dir, clip_secs, num_tags, per_duration, extract=False)
+    tag_pairs=randomtag(media_path, out_dir, clip_secs, num_tags, per_duration, extract=False)
     if extract==True:
-        tags_to_wav(media_path,basename,out_dir,tag_table)
+        tags_to_wav(media_path,out_dir,tag_pairs)
     else:
-        tags_to_csv(os.path.join(out_dir,csv_filename),tag_table,class_num=0,class_label='')
+        basename=os.path.splitext(os.path.basename(media_path))[0]
+        csv_filename=basename+"|%ss_x%sec_random.csv"%(str(clip_secs),str(num_tags))
+        if per_duration > 0:
+            csv_filename=basename+"|%ssec_x%s_per_%ssec_random.csv"%(str(clip_secs),str(num_tags),str(per_duration))
+        tags_to_csv(os.path.join(out_dir,csv_filename),tag_pairs,class_num=0,class_label='')
 
 
 
