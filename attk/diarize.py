54fb512f50bac34f31bf30360c4b5f7d043897ba
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2017-06-02 23:50:58 -0500

    small tweaks

--- attk/diarize.py
+++ attk/diarize.py
@@ -48,7 +48,9 @@ def class_list_to_time_rows(class_list,buffer_secs):
 
 
 
-def diarize(inputfile,outputfile,to_csv=True,plot=False,numSpeakers=0,buffer_secs=0):
+def diarize(inputfile,outputfile='',to_csv=True,numSpeakers=0,buffer_secs=0,plot=False):
+    if outputfile=='':
+        outputfile=inputfile[:-4]+'.diarized.csv'
     try:
         wav_source=True
         if inputfile.lower()[-4:]!='.wav':     # Creates a temporary WAV
@@ -69,7 +71,7 @@ def diarize(inputfile,outputfile,to_csv=True,plot=False,numSpeakers=0,buffer_sec
             os.remove(wav_path)
         if to_csv==True:
             if outputfile=='':
-                outputfile=inputfile[:-4]+'_diarized.csv'
+                outputfile=inputfile[:-4]+'.diarized.csv'
             with open(outputfile, 'w') as csv_fo:
                 csv_writer = csv.writer(csv_fo)
                 csv_writer.writerows(class_rows)
@@ -88,11 +90,11 @@ def main(argv):
     outputfile = ''
     plot=False
     to_csv=True
-    buffer_secs=1
+    buffer_secs=0
     numSpeakers=0
     script_path=os.path.dirname(os.path.realpath(sys.argv[0]))
     try:
-        opts, args = getopt.getopt(argv[1:],"hi:o:pn:cb",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv[1:],"hi:o:pn:cb:",["ifile=","ofile="])
     except getopt.GetoptError:
         print "FindApplause.py -i <inputfile> -o <outputfile> -p'"
         sys.exit(2)
@@ -105,25 +107,22 @@ def main(argv):
         elif opt in ("-o", "--ofile"):
             outputfile = arg
             to_csv=True
-        elif opt in ("-n"):
-            numSpeakers=arg
-    if ("-p" in sys.argv[1:]):
-        plot=True
-    if ("-c" in sys.argv[1:])|("--csv" in sys.argv[1:]):
-        to_csv=True
-    if ("-b" in sys.argv[1:])|("--buffer" in sys.argv[1:]):
-        buffer_secs=1
+        elif opt in ("-n","--numspeakers"):
+            numSpeakers = arg
+        elif opt in ("-p","--plot"):
+            plot=True
+        elif opt ("-c","--csv"):
+            to_csv=True
+        elif opt ("-b","--buffer"):
+            buffer_secs = arg
     if ('.mp3' in inputfile.lower())|('.wav' in inputfile.lower())|('.mp4' in inputfile.lower()):
-        diarize(inputfile,outputfile,to_csv,plot,numSpeakers,buffer_secs)
+        diarize(inputfile,outputfile,to_csv,numSpeakers,buffer_secs,plot)
     elif os.path.isdir(sys.argv[-1]):
         media_dir=sys.argv[-1]
         media_paths=[os.path.join(media_dir,item) for item in os.listdir(media_dir)]
         for pathname in media_paths:
             if pathname.lower()[-4:] in ('.wav','.mp3','.mp4'):
-                diarize(pathname,outputfile,to_csv,plot,numSpeakers,buffer_secs)
-
-
-
+                diarize(pathname,outputfile,to_csv,numSpeakers,buffer_secs,plot)
 
 
 
