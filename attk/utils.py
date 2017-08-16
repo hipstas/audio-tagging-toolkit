b44adef1784f69ecb896e2a37da1152b59d76a6a
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2017-08-16 00:15:51 -0500

    fixed bug in get_mfccs_and_deltas

--- attk/utils.py
+++ attk/utils.py
@@ -39,7 +39,9 @@ def get_mfccs(wav_pathname):
 
 def get_mfccs_and_deltas(wav_pathname):
     sample_array, sample_rate = librosa.load(wav_pathname)
-    if len(sample_array) > 0:
+    if len(sample_array) == 0:
+        return []
+    else:
         mfcc = librosa.feature.mfcc(sample_array, sample_rate, hop_length=2048, n_mfcc=13)
         delta = librosa.feature.delta(mfcc)
         delta2 = librosa.feature.delta(mfcc, order=2)
@@ -50,8 +52,7 @@ def get_mfccs_and_deltas(wav_pathname):
         all_features = []
         for i in range(len(mfcc)):
             all_features.append(list(mfcc_sans_0th[i]) + list(delta[i]) + list(delta2[i]))
-            return all_features
-    else: return []
+        return all_features
 
 
 
